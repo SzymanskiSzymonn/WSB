7f03404264dcdd8268ef9502204a0a6d85752fd3
SzymanskiSzymonn <82847387+SzymanskiSzymonn@users.noreply.github.com>
2023-11-24 13:24:28 +0100

    Update Program.cs

--- Odtwarzacz/Odtwarzacz/Program.cs
+++ Odtwarzacz/Odtwarzacz/Program.cs
@@ -122,7 +122,7 @@
                 Console.Write(">>");
                 Thread.Sleep(500);
             }
-            Remove(songNumber);
+            this.Remove(songNumber);
         }
     }
 
