c3fd6bf299459b86c449f567d050be6ba5307748
SzymanskiSzymonn <82847387+SzymanskiSzymonn@users.noreply.github.com>
2023-12-08 13:56:09 +0100

    Add files via upload

---
+++ Zdzarzenia/Zdzarzenia/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Zdzarzenia
+{
+    public delegate void MessageHandler(string message);
+    public class Publisher
+    {
+        public event MessageHandler MessageEvent;
+
+        public void SendMessage(string message)
+        {
+            if (MessageEvent != null)
+            {
+                MessageEvent(message);
+            }
+        }
+    }
+
+    public class Subscriber
+    {
+
+        public void OnMessageReceived(string message)
+        {
+            Console.WriteLine("Otrzymałem wiadomość: {0}", message);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            Subscriber subscriber = new Subscriber();
+            
+            publisher.MessageEvent += subscriber.OnMessageReceived;
+            publisher.SendMessage("test1");
+            publisher.SendMessage("test2");
+            publisher.SendMessage("test3");
+            publisher.SendMessage("test4");
+            publisher.MessageEvent -= subscriber.OnMessageReceived;
+            publisher.SendMessage("test5");
+        }
+    }
+}
