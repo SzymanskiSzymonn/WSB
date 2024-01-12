4e0f26378991060284cbe166e24adf03b5865033
SzymanskiSzymonn <82847387+SzymanskiSzymonn@users.noreply.github.com>
2024-01-12 12:47:29 +0100

    Add files via upload

---
+++ Serializacja/Serializacja/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { FirstName = "Franek", LastName = "Kowalski", Age = 20 };
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (FileStream fileStream = new FileStream("osoba.xml", FileMode.Create, FileAccess.Write))
+            {
+                serializer.Serialize(fileStream, person);
+            }
+        }
+    }
+}
